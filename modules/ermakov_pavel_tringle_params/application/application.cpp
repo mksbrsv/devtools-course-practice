d170a87387aa4ccc6e5e678eda3c947e5cc24a96
Maksim <arryowtf@gmail.com>
2022-05-17 19:54:31 +0300

    lab done

---
+++ modules/ermakov_pavel_tringle_params/application/application.cpp
@@ -0,0 +1,9 @@
+// Copyright 2022 Borisov Maxim
+
+#include "include/triangle_app.h"
+
+int main(int argc, char** argv) {
+  application app;
+  std::string output = app(argc, argv);
+  printf("%s\n", output.c_str());
+}
